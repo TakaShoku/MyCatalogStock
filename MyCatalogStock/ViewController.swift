06e3003d337ebf5d1ac2f8e2a24c1b15402d680c
hirotaka.s <hiro.0723boss.macpro@gmail.com>
2019-01-19 12:15:31 +0900

    Initial Commit

---
+++ MyCatalogStock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCatalogStock
+//
+//  Created by 曽和寛貴 on 2019/01/19.
+//  Copyright © 2019 曽和寛貴. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
